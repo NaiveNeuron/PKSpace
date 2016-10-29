2d7a89cca6aa04844f9d6530c28fd70753c104b2
Adrian Matejov <a.matejov@centrum.sk>
2016-10-29 16:23:56 +0200

    update: added click package for argument parsing
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- scripts/capture.py
+++ scripts/capture.py
@@ -1,19 +1,24 @@
 import cv2
 import datetime
 import os
-import sys
-import optparse
+import click
+
 
 OUTPUT_DIR = '/home/pi/data'
 DIR = '%Y-%m-%d'
 FILE = '%H:%M:%S.png'
 
 
-def capture_and_save(warmup=15):
+@click.command()
+@click.option('--warmup', default=15,
+              help='Warmup camera by capturing given number of frames')
+@click.option('--threshold', default=60,
+              help='Threshold value for grayscale to binary image')
+def capture_and_save(warmup, threshold):
     cap = cv2.VideoCapture(0)
 
     #  warmup camera
-    for i in range(15):
+    for i in range(warmup):
         _, _ = cap.read()
 
     ret, frame = cap.read()
@@ -23,7 +28,7 @@ def capture_and_save(warmup=15):
 
     grayimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    _, thresh = cv2.threshold(grayimg, 60, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(grayimg, threshold, 255, cv2.THRESH_BINARY)
     nonzero = cv2.countNonZero(thresh)
 
     if thresh.size - nonzero < thresh.size // 3:
@@ -32,14 +37,10 @@ def capture_and_save(warmup=15):
         current_time = now.strftime(FILE)
         if not os.path.exists(os.path.join(OUTPUT_DIR, current_date)):
             os.makedirs(os.path.join(OUTPUT_DIR, current_date))
-        cv2.imwrite(os.path.join(OUTPUT_DIR, current_date, current_time), frame)
+        cv2.imwrite(os.path.join(OUTPUT_DIR, current_date, current_time),
+                    frame)
     cap.release()
 
+
 if __name__ == '__main__':
-    parser = optparse.OptionParser()
-    parser.add_option('-w', '--warmup',
-                      dest='warmup',
-                      help='Warmup camera by capturing given number of frames',
-                      default='15')
-    options, _ = parser.parse_args(sys.argv[1:])
-    capture_and_save(warmup=options.warmup)
+    capture_and_save()
