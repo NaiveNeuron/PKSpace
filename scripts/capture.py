e688a70ce6bebdf43862a0b4e02d5b3d469af491
Adrian Matejov <a.matejov@centrum.sk>
2016-10-29 14:20:09 +0200

    update: get rid of 2 thresholds
    * capture only when there is less than 1/3 of
      dark pixels
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- scripts/capture.py
+++ scripts/capture.py
@@ -8,6 +8,7 @@ OUTPUT_DIR = '/home/pi/data'
 DIR = '%Y-%m-%d'
 FILE = '%H:%M:%S.png'
 
+
 def capture_and_save(warmup=15):
     cap = cv2.VideoCapture(0)
 
@@ -16,26 +17,27 @@ def capture_and_save(warmup=15):
         _, _ = cap.read()
 
     ret, frame = cap.read()
+    if not ret:
+        print('Failed to capture frame, closing...')
+        return
+
     grayimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    _, thresh = cv2.threshold(grayimg, 80, 255, cv2.THRESH_BINARY)
-    _, thresh_inv = cv2.threshold(grayimg, 80, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(grayimg, 60, 255, cv2.THRESH_BINARY)
     nonzero = cv2.countNonZero(thresh)
-    nonzero_inv = cv2.countNonZero(thresh_inv)
 
-    if nonzero - nonzero_inv > 0:
+    if thresh.size - nonzero < thresh.size // 3:
         now = datetime.datetime.now()
         current_date = now.strftime(DIR)
         current_time = now.strftime(FILE)
         if not os.path.exists(os.path.join(OUTPUT_DIR, current_date)):
             os.makedirs(os.path.join(OUTPUT_DIR, current_date))
         cv2.imwrite(os.path.join(OUTPUT_DIR, current_date, current_time), frame)
-
     cap.release()
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option('-w', '--warmup', 
+    parser.add_option('-w', '--warmup',
                       dest='warmup',
                       help='Warmup camera by capturing given number of frames',
                       default='15')
