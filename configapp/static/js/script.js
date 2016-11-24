f00bbba018849023cb0e55a06588d830cc69d774
Adrian Matejov <a.matejov@centrum.sk>
2016-11-25 00:07:33 +0100

    update: rotating clipped image
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- configapp/static/js/script.js
+++ configapp/static/js/script.js
@@ -2,14 +2,17 @@ function Polygons()
 {
     this.polygons = [];
     this.current_polygon = [];
+    this.rotation = 0;
     this.canvas = document.getElementById("canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.canvas2 = document.getElementById("canvas-mask")
+    this.ctx2 = this.canvas2.getContext("2d");
     this.rect = this.canvas.getBoundingClientRect();
     this.image = null;
 }
 
 Polygons.prototype.save_polygon = function() {
-    this.polygons.push(this.current_polygon);
+    this.polygons.push([this.current_polygon, this.rotation]);
     this.current_polygon = [];
     this.redraw();
 }
@@ -35,9 +38,12 @@ Polygons.prototype.set_image = function(image) {
 }
 
 Polygons.prototype.redraw = function() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.draw_image();
-    this.draw_polygons();
+    if (this.image !== null) {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.draw_image();
+        this.draw_polygons();
+        this.draw_mask();
+    }
 }
 
 Polygons.prototype.draw_image = function() {
@@ -62,7 +68,42 @@ Polygons.prototype.draw_polygons = function() {
     this.draw_polygon(this.current_polygon, 'red');
 
     for (var i = 0; i < this.polygons.length; i++) {
-        this.draw_polygon(this.polygons[i], '#0AFC04');
+        this.draw_polygon(this.polygons[i][0], '#0AFC04');
     }
 }
 
+Polygons.prototype.rotate_point = function(x, y) {
+    var angle = this.rotation * Math.PI / 180;
+    var _x = x*Math.cos(angle) - y*Math.sin(angle);
+    var _y = x*Math.sin(angle) + y*Math.cos(angle);
+
+    return [_x, _y];
+}
+
+Polygons.prototype.draw_mask = function() {
+    this.ctx2.clearRect(0, 0, this.canvas2.width, this.canvas2.height);
+    if (this.current_polygon.length) {
+        this.ctx2.save();
+        
+        this.ctx2.beginPath();
+        
+        var moved = this.rotate_point(this.current_polygon[0][0],
+                                      this.current_polygon[0][1]);
+        this.ctx2.moveTo(moved[0], moved[1]);
+        for (var i = 1; i < this.current_polygon.length; i++) {
+            moved = this.rotate_point(this.current_polygon[i][0],
+                                      this.current_polygon[i][1]);
+            this.ctx2.lineTo(moved[0], moved[1]);
+        }
+        this.ctx2.closePath();
+        this.ctx2.rotate(this.rotation * Math.PI / 180);
+        this.ctx2.clip();
+        this.ctx2.drawImage(this.image, 0, 0);
+        this.ctx2.restore();
+    }
+}
+
+Polygons.prototype.update_rotation = function(degrees) {
+    this.rotation = degrees;
+    this.draw_mask();
+}
