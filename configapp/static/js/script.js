7553341d372834e8d5c2833e6239e7530172c14a
Adrian Matejov <a.matejov@centrum.sk>
2016-12-17 22:44:22 +0100

    update: better user friendliness
    * user feedback on first click - fixes #4
    * moving point around - fixes #5
    * ability to remove saved polygon - fixes #6
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- configapp/static/js/script.js
+++ configapp/static/js/script.js
@@ -1,7 +1,18 @@
+function Point(x, y)
+{
+    this.x = x;
+    this.y = y;
+}
+
+Point.prototype.dump = function() {
+    return [this.x, this.y];
+}
+
 function Polygons()
 {
     this.WIDTH = 640;
     this.HEIGHT = 480;
+    this.RADIUS = 6;
 
     this.polygons = [];
     this.current_polygon = [];
@@ -13,6 +24,17 @@ function Polygons()
     this.rect = this.canvas.getBoundingClientRect();
     this.image = null;
     this.rotated = false;
+
+    this.drag_points = false;
+    this.dragging = false;
+    this.drag_point = null;
+}
+
+Polygons.prototype.start_drag_points = function() {
+    this.drag_points = !this.drag_points;
+    if (this.drag_points)
+        this.save_polygon();
+    this.redraw();
 }
 
 Polygons.prototype.save_polygon = function() {
@@ -22,14 +44,56 @@ Polygons.prototype.save_polygon = function() {
     this.redraw();
 }
 
+Polygons.prototype.contains = function(x1, y1, x2, y2) {
+    var dx = Math.abs(x1-x2);
+    var dy = Math.abs(y1-y2);
+    return Math.sqrt(dx*dx + dy*dy) < this.RADIUS;
+}
+
+Polygons.prototype.get_clicked = function(x, y) {
+    for (var i = 0; i < this.polygons.length; i++) {
+        var polygon = this.polygons[i][0];
+        for (var j = 0; j < polygon.length; j++) {
+            if (this.contains(x-this.rect.left-1, y-this.rect.top-1,
+                              polygon[j].x, polygon[j].y))
+                return polygon[j];
+        }
+    }
+    return false;
+}
+
+Polygons.prototype.stop_dragging = function() {
+    this.drag_point = null;
+    this.dragging = false;
+}
+
+Polygons.prototype.move_point = function(x, y) {
+    if (this.drag_point != null) {
+        this.drag_point.x = x-this.rect.left-1;
+        this.drag_point.y = y-this.rect.top-1;
+        this.redraw();
+    }
+}
+
 Polygons.prototype.add_point = function(x, y) {
     if (this.image == null) {
         alert("You have to choose image firstly");
         return;
     }
 
-    /* -1 because of 1px border around canvas */
-    this.current_polygon.push([x-this.rect.left-1, y-this.rect.top-1]);
+    /* if user wants to drag points, remember that one to be dragged */
+    if (this.drag_points) {
+        this.dragging = !this.dragging;
+        if (this.dragging)
+            this.drag_point = this.get_clicked(x, y);
+        else
+            this.drag_point = null;
+    } else {
+        /* -1 because of 1px border around canvas */
+        var point = new Point(x-this.rect.left-1, y-this.rect.top-1);
+        this.current_polygon.push(point)
+    }
+    this.redraw();
 }
 
 Polygons.prototype.undo = function() {
@@ -75,25 +139,37 @@ Polygons.prototype.draw_image = function() {
     }
 }
 
-Polygons.prototype.draw_polygon = function(polygon, color) {
+Polygons.prototype.draw_polygon = function(polygon, color, points) {
     if (polygon.length) {
+        this.ctx.save();
         this.ctx.beginPath();
         this.ctx.strokeStyle = color;
         this.ctx.lineWidth = 3;
-        this.ctx.moveTo(polygon[0][0], polygon[0][1]);
+        this.ctx.moveTo(polygon[0].x, polygon[0].y);
+
         for (var i = 1; i < polygon.length; i++) {
-                this.ctx.lineTo(polygon[i][0], polygon[i][1])
+                this.ctx.lineTo(polygon[i].x, polygon[i].y);
         }
         this.ctx.closePath();
         this.ctx.stroke();
+        
+        if (points) {
+            for (var i = 0; i < polygon.length; i++) {
+                this.ctx.beginPath();
+                this.ctx.arc(polygon[i].x, polygon[i].y, this.RADIUS,
+                             0, 2*Math.PI);
+                this.ctx.closePath();
+                this.ctx.stroke();
+            }
+        }
     }
 }
 
 Polygons.prototype.draw_polygons = function() {
-    this.draw_polygon(this.current_polygon, 'red');
+    this.draw_polygon(this.current_polygon, 'red', true);
 
     for (var i = 0; i < this.polygons.length; i++) {
-        this.draw_polygon(this.polygons[i][0], '#0AFC04');
+        this.draw_polygon(this.polygons[i][0], '#0AFC04', this.drag_points);
     }
 }
 
@@ -118,19 +194,19 @@ Polygons.prototype.draw_mask = function() {
         var centerx = 0;
         var centery = 0;
         for (var i = 0; i < this.current_polygon.length; i++){
-            centerx += this.current_polygon[i][0];
-            centery += this.current_polygon[i][1];
+            centerx += this.current_polygon[i].x;
+            centery += this.current_polygon[i].y;
         }
         centerx = centerx / this.current_polygon.length;
         centery = centery / this.current_polygon.length;
 
-        var moved = this.rotate_point(this.current_polygon[0][0],
-                                      this.current_polygon[0][1],
+        var moved = this.rotate_point(this.current_polygon[0].x,
+                                      this.current_polygon[0].y,
                                       centerx, centery);
         this.ctx2.moveTo(moved[0], moved[1]);
         for (var i = 1; i < this.current_polygon.length; i++) {
-            moved = this.rotate_point(this.current_polygon[i][0],
-                                      this.current_polygon[i][1],
+            moved = this.rotate_point(this.current_polygon[i].x,
+                                      this.current_polygon[i].y,
                                       centerx, centery);
             this.ctx2.lineTo(moved[0], moved[1]);
         }
