d6ea5b2ee153df6d4bc4dbbbd1cca8a80cec8e3f
Adrian Matejov <a.matejov@centrum.sk>
2016-12-30 23:59:38 +0100

    update: python 3 compatible
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- scripts/capture.py
+++ scripts/capture.py
@@ -10,7 +10,7 @@ FILE = '%H:%M:%S.png'
 
 def rotate_image(image, angle):
     (h, w) = image.shape[:2]
-    center = (w / 2, h / 2)
+    center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
     return cv2.warpAffine(image, M, (w, h))
 
