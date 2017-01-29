7be4551861793e9dcc7a9515fe3ca94690f53c93
Adrian Matejov <a.matejov@centrum.sk>
2017-01-29 22:26:31 +0100

    initial of labeler app -- doesnt work yet
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- configapp/configapp.py
+++ configapp/configapp.py
@@ -1,28 +1,72 @@
 import os
+import json
 
-from flask import Flask, render_template, send_from_directory
+from flask import (Flask, render_template, send_from_directory, request,
+                   redirect, url_for)
 
 app = Flask(__name__)
 app.config.from_object('configapp.default_settings')
 
 filedir = os.path.dirname(os.path.abspath(__file__))
 directory = os.path.join(filedir, app.config['IMAGE_PATH'])
+dataset_dir = os.path.join(filedir, app.config['DATASET_PATH'])
 
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/marker', methods=['GET', 'POST'])
+def marker():
+    if request.method == 'POST':
+        if 'output' in request.form:
+            polygons = request.form.get('output')
+            with open(os.path.join(filedir, 'polygons.json'), 'w+') as f:
+                f.write(polygons)
+            return redirect(url_for('labeler'))
+
     files = []
     listdir = os.listdir(directory)
     for f in listdir:
         if f.endswith(app.config['IMAGE_SUFFIX']):
             files.append(f)
 
-    return render_template('index.html', images=files)
+    return render_template('marker.html', images=files)
+    
+
+@app.route('/labeler', methods=['GET', 'POST'])
+def labeler():
+    try:
+        with open(os.path.join(filedir, 'polygons.json')) as f:
+            data = json.load(f)
+        polygons = data['polygons']
+    except IOError:
+        return render_template('labeler.html', nojson=True)
+
+    data = []
+    # we assume that directory walked contains DATE/TIME.png files
+    # othewise it may crash -- needs some testing
+    subdirs = [d for d in os.listdir(dataset_dir)
+                    if os.path.isdir(os.path.join(dataset_dir, d))]
+            
+    for subdir in subdirs:
+        for f in os.listdir(os.path.join(dataset_dir, subdir)):
+            if f.endswith(app.config['IMAGE_SUFFIX']):
+                data.append(os.path.join(subdir, f))
+
+    return render_template('labeler.html', imgs=data)
 
 
 @app.route('/image/<path:filename>')
 def data_image(filename):
     return send_from_directory(directory, filename)
+
+
+@app.route('/datasetimg/<path:img>')
+def dataset_image(img):
+    img = img.split('/')
+    return send_from_directory(os.path.join(dataset_dir, img[0]), img[1])
 
 
 if __name__ == '__main__':
