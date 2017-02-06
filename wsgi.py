513632cac0eb4fa0a0173f364acdbadc79bfdc06
Adrian Matejov <a.matejov@centrum.sk>
2017-02-06 15:22:49 +0100

    pep8 janitoring
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR', '.'), 'virtenv')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
