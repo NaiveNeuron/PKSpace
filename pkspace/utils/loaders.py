e7dc006b65a691e50f172219021ed5c1749ba930
Roman Števaňák <rstevanak@gmail.com>
2017-02-06 16:28:16 +0100

    hopefully fixed pep8
    Signed-off-by: Roman Števaňák <rstevanak@gmail.com>

--- pkspace/utils/loaders.py
+++ pkspace/utils/loaders.py
@@ -2,10 +2,8 @@ import glob
 import json
 import os
 import xml.etree.ElementTree
-
 import cv2
 import numpy as np
-import scipy.ndimage
 
 
 class Loader:
@@ -21,38 +19,36 @@ class PKspaceLoader(Loader):
         for space in desc.get('spots'):
             points = space.get('points')
             angle = space.get('rotation')
-            nove_pole = np.hstack((np.asarray(points), np.ones((len(points), 1))))
-            sum_x, sum_y = 0, 0
-            for x, y in points:
-                sum_x += x
-                sum_y += y
-            center = np.ceil(sum_x / len(points)), np.ceil(sum_y / len(points))
-            # if(center[0]!=int(np.mean(nove_pole,0)[0]) or center[1]!=int(np.mean(nove_pole, 0)[1])):
-            #     print(center[0],np.mean(nove_pole,0),center[1],np.mean(nove_pole,0))
+            nove_pole = np.hstack(
+                (np.asarray(points), np.ones((len(points), 1))))
+            center = np.mean(nove_pole, 0)[:2]
             rows, cols, tmp = src.shape
-            min_x, min_y = rows,cols
-            max_x, max_y = 0, 0
-            for x, y in points:
-                xrotated = ((x - center[0]) * np.cos(np.math.radians(angle))) - (
-                (y - center[1]) * np.sin(np.math.radians(angle))) + center[0]
-                yrotated = ((x - center[0]) * np.sin(np.math.radians(angle))) + (
-                (y - center[1]) * np.cos(np.math.radians(angle))) + center[1]
-                min_x, min_y = max(min(min_x, int(xrotated)), 0), max(min(min_y, int(yrotated)), 0)
-                max_x, max_y = max(max_x, int(xrotated)), max(max_y, int(yrotated))
 
-            M = cv2.getRotationMatrix2D(center, -angle, 1)
-            # nove_pole = M.dot(nove_pole.T).T
-            #
+            min_x, min_y = rows, cols
+            max_x, max_y = 0, 0
+            rad = np.math.radians(angle)
+            for x, y in points:
+                x_rotated = ((x - center[0]) * np.cos(rad)) \
+                    - ((y - center[1]) * np.sin(rad)) + center[0]
+                y_rotated = ((x - center[0]) * np.sin(rad)) \
+                    + ((y - center[1]) * np.cos(rad)) + center[1]
+                min_x, min_y = max(min(min_x, int(x_rotated)), 0), \
+                    max(min(min_y, int(y_rotated)), 0)
+                max_x, max_y = max(max_x, int(x_rotated)), \
+                    max(max_y, int(y_rotated))
+
+            m = cv2.getRotationMatrix2D(center, -angle, 1)
+            # nove_pole = m.dot(nove_pole.T).T
             # nove_pole[:, 0] = np.clip(nove_pole[:, 0], 0, rows)
             # nove_pole[:, 1] = np.clip(nove_pole[:, 1], 0, cols)
             # nove_pole = nove_pole.astype('uint8')
             # minima = nove_pole.min(axis=0)
             # maxima = nove_pole.max(axis=0)
-            # # print(nove_pole)
+            # print(nove_pole)
             # min_x, min_y = minima[:2]
             # max_x, max_y = maxima[:2]
 
-            rotated = cv2.warpAffine(src, M, (cols, rows))
+            rotated = cv2.warpAffine(src, m, (cols, rows))
             roi = rotated[min_y:max_y, min_x:max_x]
             fin = cv2.resize(roi, finsize).flatten()
             spaces.append(fin)
@@ -65,7 +61,8 @@ class PKspaceLoader(Loader):
         extension = "*{}".format(extension)
         for file in glob.glob(os.path.join(path, extension)):
             img = cv2.imread(file)
-            with open("{}.json".format(os.path.splitext(file)[0])) as data_file:
+            with open("{}.json".format(os.path.splitext(file)[0])) \
+                    as data_file:
                 desc = json.load(data_file)
             spaces, answers = self.__get_spaces__(img, desc)
             all_parking_spaces.append(spaces)
@@ -90,10 +87,12 @@ class PKlotLoader(Loader):
         for space in tabroot:
             center = int(space[0][0].get('x')), int(space[0][0].get('y'))
             h, w = int(space[0][1].get('h')), int(space[0][1].get('w'))
-            M = cv2.getRotationMatrix2D(center, int(space[0][2].get('d')), 1)
+            m = cv2.getRotationMatrix2D(center, int(space[0][2].get('d')), 1)
             rows, cols, tmp = src.shape
-            dst = cv2.warpAffine(src, M, (cols, rows))
-            roi = dst[int(center[1] - h / 2):int(center[1] + h / 2), int(center[0] - w / 2):int(center[0] + w / 2)]
+            dst = cv2.warpAffine(src, m, (cols, rows))
+            roi = dst[
+                  int(center[1] - h / 2):int(center[1] + h / 2),
+                  int(center[0] - w / 2):int(center[0] + w / 2)]
             fin = cv2.resize(roi, fin_size).flatten()
             answers.append(space.get('occupied', 1))
             spaces.append(fin)
@@ -105,7 +104,8 @@ class PKlotLoader(Loader):
         extension = "*{}".format(extension)
         for file in glob.glob(os.path.join(path, extension)):
             img = cv2.imread(file)
-            e = xml.etree.ElementTree.parse("{}.xml".format(os.path.splitext(file)[0]))
+            e = xml.etree.ElementTree.parse(
+                "{}.xml".format(os.path.splitext(file)[0]))
             root = e.getroot()
             spaces, answers = self.__get_spaces__(img, root)
             all_parking_spaces.append(spaces)
--- pkspace/utils/utils.py
+++ pkspace/utils/utils.py
@@ -4,6 +4,7 @@ from pkspace.utils import loaders
 def load_dataset(loader, path, split):
     return loader.load(path, split)
 
+
 if __name__ == '__main__':
     loader = loaders.PKspaceLoader()
     a, b, c, d = load_dataset(loader, 'labeled_data/2017-01-10', 0.2)
