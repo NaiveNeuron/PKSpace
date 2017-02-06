a21e3fec0586951329bcbc0bb1a0026f5b7d590f
Roman Števaňák <rstevanak@gmail.com>
2017-02-06 16:16:43 +0100

    Hopefully fixed pep8 warnings
    Signed-off-by: Roman Števaňák <rstevanak@gmail.com>

--- pkspace/utils/utils.py
+++ pkspace/utils/utils.py
@@ -1,19 +1,8 @@
-import os
-
-import cv2
-
 from pkspace.utils import loaders
 
 
 def load_dataset(loader, path, split):
     return loader.load(path, split)
-
-
-def visual(x_train, y_train):
-    for picture in x_train:
-        cv2.imshow('image', picture)
-        cv2.waitKey(0)
-
 
 if __name__ == '__main__':
     loader = loaders.PKspaceLoader()
