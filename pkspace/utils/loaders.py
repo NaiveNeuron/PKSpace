6f2fd9ea72c3c87c132a26443ad75d84b87a09fa
Roman Števaňák <rstevanak@gmail.com>
2017-02-07 14:29:00 +0100

    Removed split
    Signed-off-by: Roman Števaňák <rstevanak@gmail.com>

--- pkspace/utils/loaders.py
+++ pkspace/utils/loaders.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 class Loader:
-    def load(self, path, split):
+    def load(self, path):
         pass
 
 
@@ -39,11 +39,11 @@ class PKSpaceLoader(Loader):
             answers.append(space['occupied'])
         return spaces, answers
 
-    def load(self, path, split, extension='.png', size=(80, 80)):
+    def load(self, path, extension='.png', size=(80, 80)):
         all_spaces = []
         all_answers = []
         extension = "*{}".format(extension)
-        for file in glob.glob(os.path.join(path, extension)):
+        for file in glob.glob(os.path.join(path, '*', extension)):
             img = cv2.imread(file)
             file_name_base = os.path.splitext(file)[0]
             json_file = open("{}.json".format(file_name_base))
@@ -55,26 +55,6 @@ class PKSpaceLoader(Loader):
         all_answers = np.asarray(all_answers)
         all_spaces = all_spaces.reshape(-1, all_spaces.shape[-1])
         all_answers = all_answers.reshape(-1)
-
-        """"
-        assert split < 1, 'split needs to be smaller than 1'
-
-        train_size = int(np.ceil(len(all_spaces) * split))
-        indices = np.random.permutation(len(all_spaces))
-
-        # dividing days into training and testing sets
-        x_train_days = all_spaces[indices[:train_size]]
-        y_train_days = all_answers[indices[:train_size]]
-        x_test_days = all_spaces[indices[:-train_size]]
-        y_test_days = all_answers[indices[:-train_size]]
-
-        # reshaping them into 2D
-        x_train = x_train_days.reshape(-1, x_train_days.shape[-1])
-        y_train = y_train_days.reshape(-1)
-        x_test = x_test_days.reshape(-1, x_test_days.shape[-1])
-        y_test = y_test_days.reshape(-1)
-        return x_train, x_test, y_train, y_test
-        """
         return all_spaces, all_answers
 
 
@@ -83,56 +63,39 @@ class PKLotLoader(Loader):
         spaces = []
         answers = []
         for space in tab_root:
-            center = int(space[0][0]['x']), int(space[0][0]['y'])
-            h, w = int(space[0][1]['h']), int(space[0][1]['w'])
+            center = int(space[0][0].get('x')), int(space[0][0].get('y'))
+            h, w = int(space[0][1].get('h')), int(space[0][1].get('w'))
             angle = float(space[0][2].get('d', 0))
 
             m = cv2.getRotationMatrix2D(center, angle, 1)
             rows, cols, _ = src.shape
             rotated = cv2.warpAffine(src, m, (cols, rows))
 
-            min_x, max_x = int(center[0] - w / 2), int(center[0] + w / 2)
-            min_y, max_y = int(center[1] - h / 2), int(center[1] + h / 2)
-            roi = rotated[min_y:max_y, min_x, max_x]
+            min_x = max(int(center[0] - w / 2), 0)
+            min_y = max(int(center[1] - h / 2), 0)
+            max_x = min(int(center[0] + w / 2), cols)
+            max_y = min(int(center[1] + h / 2), rows)
+            roi = rotated[min_y:max_y, min_x:max_x]
             fin = cv2.resize(roi, fin_size).flatten()
             answers.append(space.get('occupied', 1))
             spaces.append(fin)
         return spaces, answers
 
-    def load(self, path, split=0.5, extension='.jpg', size=(80, 80)):
+    def load(self, path, extension='.jpg', size=(80, 80)):
         all_spaces = []
         all_answers = []
         extension = "*{}".format(extension)
-        for file in glob.glob(os.path.join(path, extension)):
+        for file in glob.glob(os.path.join(path, '*', '*', extension)):
             img = cv2.imread(file)
-            xml_name_base = os.path.splitext(file)[0]
-            e = xml.etree.ElementTree.parse("{}.xml".format(xml_name_base))
-            root = e.getroot()
-            spaces, answers = self._get_spaces(img, root, size)
-            all_spaces.append(spaces)
-            all_answers.append(answers)
+            xml_filename = "{}.xml".format(os.path.splitext(file)[0])
+            if os.path.isfile(xml_filename):
+                e = xml.etree.ElementTree.parse(xml_filename)
+                root = e.getroot()
+                spaces, answers = self._get_spaces(img, root, size)
+                all_spaces.append(spaces)
+                all_answers.append(answers)
         all_spaces = np.asarray(all_spaces)
         all_answers = np.asarray(all_answers)
         all_spaces = all_spaces.reshape(-1, all_spaces.shape[-1])
         all_answers = all_answers.reshape(-1)
-
-        """"
-        assert split < 1, 'split needs to be smaller than 1'
-
-        train_size = int(np.ceil(len(all_spaces) * split))
-        indices = np.random.permutation(len(all_spaces))
-
-        # dividing days into training and testing sets
-        x_train_days = all_spaces[indices[:train_size]]
-        y_train_days = all_answers[indices[:train_size]]
-        x_test_days = all_spaces[indices[:-train_size]]
-        y_test_days = all_answers[indices[:-train_size]]
-
-        # reshaping them into 2D
-        x_train = x_train_days.reshape(-1, x_train_days.shape[-1])
-        y_train = y_train_days.reshape(-1)
-        x_test = x_test_days.reshape(-1, x_test_days.shape[-1])
-        y_test = y_test_days.reshape(-1)
-        return x_train, x_test, y_train, y_test
-        """
         return all_spaces, all_answers
