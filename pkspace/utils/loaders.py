edaad2d8868d9aa4d1b7b8af7f040c28434d1e7f
Roman Števaňák <rstevanak@gmail.com>
2017-03-30 23:21:55 +0200

    added prediction, added and reformated loaders

--- pkspace/utils/loaders.py
+++ pkspace/utils/loaders.py
@@ -10,6 +10,9 @@ class Loader:
     def load(self, path):
         pass
 
+    def load_pic(self, pic_path, desc_path, fin_size):
+        pass
+
 
 class PKSpaceLoader(Loader):
     def _get_spaces(self, src, desc, fin_size):
@@ -36,7 +39,7 @@ class PKSpaceLoader(Loader):
             roi = rotated[min_y:max_y, min_x:max_x]
             fin = cv2.resize(roi, fin_size).flatten()
             spaces.append(fin)
-            answers.append(space['occupied'])
+            answers.append(space.get('occupied'))
         return spaces, answers
 
     def load(self, path, extension='.png', size=(80, 80)):
@@ -44,11 +47,9 @@ class PKSpaceLoader(Loader):
         all_answers = []
         extension = "*{}".format(extension)
         for file in glob.glob(os.path.join(path, '*', extension)):
-            img = cv2.imread(file)
             file_name_base = os.path.splitext(file)[0]
-            json_file = open("{}.json".format(file_name_base))
-            desc = json.load(json_file)
-            spaces, answers = self._get_spaces(img, desc, size)
+            json_filename = "{}.json".format(file_name_base)
+            spaces, answers = self.load_pic(file, json_filename, size)
             all_spaces.append(spaces)
             all_answers.append(answers)
         all_spaces = np.asarray(all_spaces)
@@ -56,6 +57,12 @@ class PKSpaceLoader(Loader):
         all_spaces = all_spaces.reshape(-1, all_spaces.shape[-1])
         all_answers = all_answers.reshape(-1)
         return all_spaces, all_answers
+
+    def load_pic(self, pic_path, desc_path, fin_size=(80, 80)):
+        img = cv2.imread(pic_path)
+        json_file = open(desc_path)
+        desc = json.load(json_file)
+        return self._get_spaces(img, desc, fin_size)
 
 
 class PKLotLoader(Loader):
@@ -86,12 +93,9 @@ class PKLotLoader(Loader):
         all_answers = []
         extension = "*{}".format(extension)
         for file in glob.glob(os.path.join(path, '*', '*', extension)):
-            img = cv2.imread(file)
             xml_filename = "{}.xml".format(os.path.splitext(file)[0])
             if os.path.isfile(xml_filename):
-                e = xml.etree.ElementTree.parse(xml_filename)
-                root = e.getroot()
-                spaces, answers = self._get_spaces(img, root, size)
+                spaces, answers = self.load_pic(file, xml_filename)
                 all_spaces.append(spaces)
                 all_answers.append(answers)
         all_spaces = np.asarray(all_spaces)
@@ -99,3 +103,9 @@ class PKLotLoader(Loader):
         all_spaces = all_spaces.reshape(-1, all_spaces.shape[-1])
         all_answers = all_answers.reshape(-1)
         return all_spaces, all_answers
+
+    def load_pic(self, pic_path, desc_path, fin_size=(80, 80)):
+        img = cv2.imread(pic_path)
+        e = xml.etree.ElementTree.parse(desc_path)
+        root = e.getroot()
+        return self._get_spaces(img, root, fin_size)
