7c431a6765a0ee6bf389f7b77a0ca447a969eef7
Roman Števaňák <rstevanak@gmail.com>
2017-04-25 18:44:42 +0200

    rstevanak\Prediction (#26)
    * added prediction, added and reformated loaders
    
    * fixed minor pep-8 problems
    
    * fixed minor error in importing
    
    * added FMPH dataset, pretrained models and fixed pep-8 in predict

---
+++ scripts/predict.py
@@ -0,0 +1,48 @@
+import json
+import os
+import pickle
+import sys
+from os.path import dirname, abspath
+sys.path.append(dirname(dirname(abspath(__file__))))
+from pkspace.utils.loaders import PKSpaceLoader # noqa
+import click # noqa
+
+
+@click.command()
+@click.option('--mask_path', required=True,
+              help='Adress of json file of the mask')
+@click.option('--picture_path', required=True,
+              help='Adress of picture to be predicted')
+@click.option('--model_path', required=True,
+              help='Path where pretrained model')
+@click.option('--output', default=None,
+              help='Name of output file for predicted output')
+def load_and_predict(mask_path, picture_path, model_path, output):
+    for file in (mask_path, picture_path, model_path):
+        if not os.path.exists(file):
+            print(file, "Path does not exist")
+            return
+    if output is None:
+        output = "{}_out.json".format(os.path.splitext(picture_path)[-2])
+
+    if os.path.splitext(mask_path)[-1] == ".json":
+        loader = PKSpaceLoader()
+        pictures, _ = loader.load_pic(picture_path, mask_path)
+    else:
+        print("Unknown description file")
+        return
+    with open(model_path, "rb") as fp:
+        model = pickle.load(fp)
+    answers = model.predict(pictures)
+    with open(mask_path, 'r') as fp:
+        input_file = json.load(fp)
+    count = 0
+    for spot in input_file['spots']:
+        spot['occupied'] = answers[count].item()
+        count += 1
+    with open(output, 'w') as fp:
+        json.dump(input_file, fp)
+
+
+if __name__ == '__main__':
+    load_and_predict()
