ce4ff4905573f459f10de0ffa2ade037ae891376
Adrián Matejov <a.matejov@centrum.sk>
2017-06-21 20:27:28 +0200

    Adman/predict fix (#31)
    * predict: option for specifying predict dir
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: output is now only the filename
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: checking for output directory presence
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: image suffix - optional parameter
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: removed predict_dir param
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: force click to check for path existence
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: better output name modification
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: directories forbidden in click arguments
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: resolve_path before checking for existence
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>
    
    * predict: got rid of img_suffix
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,45 +1,63 @@
+from __future__ import print_function
 import json
 import os
+import re
 import pickle
 import sys
-from os.path import dirname, abspath
-sys.path.append(dirname(dirname(abspath(__file__))))
+import click
+import imghdr
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pkspace.utils.loaders import PKSpaceLoader # noqa
-import click # noqa
 
 
 @click.command()
 @click.option('--mask_path', required=True,
-              help='Adress of json file of the mask')
+              type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+              help='Path of json file of the mask')
 @click.option('--picture_path', required=True,
-              help='Adress of picture to be predicted')
+              type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+              help='Path of picture to be predicted')
 @click.option('--model_path', required=True,
-              help='Path where pretrained model')
+              type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+              help='Path where pretrained model is located')
 @click.option('--output', default=None,
-              help='Name of output file for predicted output')
+              help='Name of file for predicted output')
 def load_and_predict(mask_path, picture_path, model_path, output):
-    for file in (mask_path, picture_path, model_path):
-        if not os.path.exists(file):
-            print(file, "Path does not exist")
-            return
-    if output is None:
-        output = "{}_out.json".format(os.path.splitext(picture_path)[-2])
+    if imghdr.what(picture_path) is None:
+        print('Picture specified is not a valid image.', file=sys.stderr)
+        sys.exit(1)
 
-    if os.path.splitext(mask_path)[-1] == ".json":
+    if output is None:
+        output = '{}_out.json'.format(os.path.splitext(picture_path)[-2])
+    else:
+        extension = os.path.splitext(output)[-1]
+        if extension == '':
+            output = '{0}{1}'.format(output, '.json')
+        else:
+            output = re.sub(extension + '$', '.json', output)
+
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    if os.path.splitext(mask_path)[-1] == '.json':
         loader = PKSpaceLoader()
         pictures, _ = loader.load_pic(picture_path, mask_path)
     else:
-        print("Unknown description file")
-        return
-    with open(model_path, "rb") as fp:
+        print('Unknown description file.', file=sys.stderr)
+        sys.exit(1)
+
+    with open(model_path, 'rb') as fp:
         model = pickle.load(fp)
     answers = model.predict(pictures)
+
     with open(mask_path, 'r') as fp:
         input_file = json.load(fp)
+
     count = 0
     for spot in input_file['spots']:
         spot['occupied'] = answers[count].item()
         count += 1
+
     with open(output, 'w') as fp:
         json.dump(input_file, fp)
 
