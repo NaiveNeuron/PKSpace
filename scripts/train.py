0029aa37c54d405be4a829d88915bb90137538f5
Roman Števaňák <rstevanak@gmail.com>
2017-06-21 13:03:38 +0200

    added parameter to specify hidden layer in MLP, shortened short forms of parameter options

--- scripts/train.py
+++ scripts/train.py
@@ -9,17 +9,17 @@ from pkspace.utils.loaders import PKSpaceLoader, PKLotLoader
 @click.command()
 @click.option('--loader', '-l', type=click.Choice(['PKLot', 'PKSpace']),
               default='PKSpace', help='Loader used to load dataset')
-@click.option('--model_type', '-mt', type=click.Choice(['MLP']), default='MLP',
+@click.option('--model_type', '-t', type=click.Choice(['MLP']), default='MLP',
               help='Type of model to be trained')
-# @click.option('--hidden_later', '-hl', default=(15, 10),
-#               help='Hidden layers of MLP, if MLP is chosen as model_type')
-@click.option('--model_path', '-pm', type=click.Path(exists=True),
+@click.option('--hidden_layer', '-h', default="15 10",
+              help='Hidden layers of MLP, if MLP is chosen as model_type')
+@click.option('--model_path', '-p', type=click.Path(exists=True),
               default=None,
               help='Path to trained model, to be used as a base in training')
 @click.argument('dataset_dir', type=click.Path(exists=True))
 @click.option('--output', '-o', default='out.pkl',
               help='Path to output file for trained model')
-def train(loader, model_type, model_path, dataset_dir, output):
+def train(loader, model_type, hidden_layer, model_path, dataset_dir, output):
     if not os.path.isdir(dataset_dir):
         sys.stderr.write('{} is not a directory'.format(dataset_dir))
         sys.exit(1)
@@ -29,21 +29,19 @@ def train(loader, model_type, model_path, dataset_dir, output):
     elif loader == 'PKLot':
         loader = PKLotLoader()
 
-    spaces, answers = loader.load(dataset_dir)
-
     if model_path is not None:
         model = joblib.load(model_path)
 
-        if not callable(getattr(model, 'partial_fit', None)):
-            sys.stderr.write('{} is not further trainable'.format(model_path))
-            sys.exit(1)
-
-        trained_model = model.partial_fit(spaces, answers)
-
     elif model_type == 'MLP':
-        model = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(15, 10))
+        layers = hidden_layer.split()
+        try:
+            layers = [int(x) for x in layers]
+        except ValueError:
+            sys.stderr.write("--hidden_layer must be ints separated by spaces")
+        model = MLPClassifier(solver='lbfgs', hidden_layer_sizes=layers)
 
-        trained_model = model.partial_fit(spaces, answers, [0, 1])
+    spaces, answers = loader.load(dataset_dir)
+    trained_model = model.fit(spaces, answers)
 
     joblib.dump(trained_model, output, protocol=0)
 
