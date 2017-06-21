83ff814dc8ce1490133f635c9566cee9fdb97eed
Roman Števaňák <rstevanak@gmail.com>
2017-06-21 13:32:41 +0200

    changed hidden layer input style

--- scripts/train.py
+++ scripts/train.py
@@ -11,19 +11,17 @@ from pkspace.utils.loaders import PKSpaceLoader, PKLotLoader
               default='PKSpace', help='Loader used to load dataset')
 @click.option('--model_type', '-t', type=click.Choice(['MLP']), default='MLP',
               help='Type of model to be trained')
-@click.option('--hidden_layer', '-h', default="15 10",
+@click.option('--hidden_layer', '-h', default=(15, 10), multiple=True,
+              type=int,
               help='Hidden layers of MLP, if MLP is chosen as model_type')
-@click.option('--model_path', '-p', type=click.Path(exists=True),
-              default=None,
+@click.option('--model_path', '-p', default=None,
+              type=click.Path(exists=True, dir_okay=False, file_okay=True),
               help='Path to trained model, to be used as a base in training')
-@click.argument('dataset_dir', type=click.Path(exists=True))
-@click.option('--output', '-o', default='out.pkl',
+@click.argument('dataset_dir',
+                type=click.Path(exists=True, file_okay=False, dir_okay=True))
+@click.option('--output', '-o', default='out.pkl', type=click.Path(),
               help='Path to output file for trained model')
 def train(loader, model_type, hidden_layer, model_path, dataset_dir, output):
-    if not os.path.isdir(dataset_dir):
-        sys.stderr.write('{} is not a directory'.format(dataset_dir))
-        sys.exit(1)
-
     if loader == 'PKSpace':
         loader = PKSpaceLoader()
     elif loader == 'PKLot':
@@ -33,12 +31,7 @@ def train(loader, model_type, hidden_layer, model_path, dataset_dir, output):
         model = joblib.load(model_path)
 
     elif model_type == 'MLP':
-        layers = hidden_layer.split()
-        try:
-            layers = [int(x) for x in layers]
-        except ValueError:
-            sys.stderr.write("--hidden_layer must be ints separated by spaces")
-        model = MLPClassifier(solver='lbfgs', hidden_layer_sizes=layers)
+        model = MLPClassifier(solver='lbfgs', hidden_layer_sizes=hidden_layer)
 
     spaces, answers = loader.load(dataset_dir)
     trained_model = model.fit(spaces, answers)
