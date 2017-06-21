9c32e26139a485c9ec0befc40ba4fe22df9fc2e5
Roman Števaňák <rstevanak@gmail.com>
2017-06-21 20:06:56 +0200

    click now checks input paths, changed pickle to joblib

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,27 +1,25 @@
-import os
-import pickle
 import click
 from sklearn.metrics import classification_report
+from sklearn.externals import joblib
 from pkspace.utils.loaders import PKSpaceLoader, PKLotLoader
 
 
 @click.command()
 @click.option('--loader', '-l', type=click.Choice(['PKLot', 'PKSpace']),
               default='PKSpace', help='Loader used to load dataset')
-@click.argument('dataset_dir')
-@click.argument('model_file')
+@click.argument('dataset_dir',
+                type=click.Path(exists=True, file_okay=False, dir_okay=True,
+                                resolve_path=True))
+@click.argument('model_file',
+                type=click.Path(exists=True, file_okay=True, dir_okay=False,
+                                resolve_path=True))
 def test_model(loader, dataset_dir, model_file):
-    if not os.path.isdir(dataset_dir):
-        print('{} is not a directory')
-        return
-
     if loader == 'PKSpace':
         loader = PKSpaceLoader()
     elif loader == 'PKLot':
         loader = PKLotLoader()
 
-    with open(model_file, 'rb') as mp:
-        model = pickle.load(mp)
+    model = joblib.load(model_file)
     spaces, ground_answers = loader.load(dataset_dir)
     model_answers = model.predict(spaces)
     print(classification_report(ground_answers, model_answers))
