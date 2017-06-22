62f5df65ed7c00eaf84f1dfa7ce50e50778b5234
Roman Števaňák <rstevanak@gmail.com>
2017-06-22 10:50:50 +0200

    Added machine friendly printing

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,6 +1,9 @@
+import json
 import click
+from collections import Counter
 from sklearn.metrics import classification_report
 from sklearn.externals import joblib
+from sklearn.metrics import f1_score, recall_score, precision_score
 from pkspace.utils.loaders import PKSpaceLoader, PKLotLoader
 
 
@@ -13,7 +16,9 @@ from pkspace.utils.loaders import PKSpaceLoader, PKLotLoader
 @click.argument('model_file',
                 type=click.Path(exists=True, file_okay=True, dir_okay=False,
                                 resolve_path=True))
-def test_model(loader, dataset_dir, model_file):
+@click.option("--machine_friendly", '-f', is_flag=True,
+              help='prints serialized dictionary of results')
+def test_model(loader, dataset_dir, model_file, machine_friendly):
     if loader == 'PKSpace':
         loader = PKSpaceLoader()
     elif loader == 'PKLot':
@@ -22,7 +27,29 @@ def test_model(loader, dataset_dir, model_file):
     model = joblib.load(model_file)
     spaces, ground_answers = loader.load(dataset_dir)
     model_answers = model.predict(spaces)
-    print(classification_report(ground_answers, model_answers))
+    if machine_friendly:
+        answer = {'avg': {}, 0: {}, 1: {}}
+        metrics = [precision_score, recall_score, f1_score]
+        classes_counter = Counter(ground_answers)
+        for i in [0, 1]:
+            for func in metrics:
+                score = func(ground_answers, model_answers, pos_label=i)
+                answer[i][func.__name__] = score
+            class_support = classes_counter[i]
+            answer[i]['support'] = class_support
+            old_sum_support = answer['avg'].get('support', 0)
+            answer['avg']['support'] = old_sum_support + class_support
+
+        for column in [x.__name__ for x in metrics]:
+            col_sum = 0
+            for ans_class in [0, 1]:
+                row = answer[ans_class]
+                col_sum += row[column] * row['support']
+            answer['avg'][column] = col_sum / answer['avg']['support']
+        print(json.dumps(answer))
+
+    else:
+        print(classification_report(ground_answers, model_answers))
 
 
 if __name__ == '__main__':
