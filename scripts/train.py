2b2789c8a5a44f28d0f032bc12af31721fe5f9c3
Roman Števaňák <rstevanak@gmail.com>
2017-06-22 10:15:14 +0200

    Rstevanak/models trainer (#35)
    * added models training
    
    * reformatted pep-8 errors
    
    * code fixes
    
    * changed loader short from -t to -l
    
    * fixed stderr output for python 2
    
    * added training of pretrained model
    
    * fixed pep-8 errors
    
    * changed pickle to joblib, transferred train functionality from utils into script and changed fit to partial_fit
    
    * added parameter to specify hidden layer in MLP, shortened short forms of parameter options
    
    * changed hidden layer input style
    
    * fixed imports
    
    * added resolve_path to click path checking
    
    * ad-hoc solution for importing neighboring package
    
    * removed explicit click path arguments

---
+++ scripts/train.py
@@ -0,0 +1,45 @@
+import os
+import click
+import sys
+from sklearn.externals import joblib
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from sklearn.neural_network import MLPClassifier # noqa
+from pkspace.utils.loaders import PKSpaceLoader, PKLotLoader # noqa
+
+
+@click.command()
+@click.option('--loader', '-l', type=click.Choice(['PKLot', 'PKSpace']),
+              default='PKSpace', help='Loader used to load dataset')
+@click.option('--model_type', '-t', type=click.Choice(['MLP']), default='MLP',
+              help='Type of model to be trained')
+@click.option('--hidden_layer', '-h', default=(15, 10), multiple=True,
+              type=int,
+              help='Hidden layers of MLP, if MLP is chosen as model_type')
+@click.option('--model_path', '-p', default=None,
+              type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+              help='Path to trained model, to be used as a base in training')
+@click.argument('dataset_dir',
+                type=click.Path(exists=True, file_okay=False,
+                                resolve_path=True))
+@click.option('--output', '-o', default='out.pkl', type=click.Path(),
+              help='Path to output file for trained model')
+def train(loader, model_type, hidden_layer, model_path, dataset_dir, output):
+    if loader == 'PKSpace':
+        loader = PKSpaceLoader()
+    elif loader == 'PKLot':
+        loader = PKLotLoader()
+
+    if model_path is not None:
+        model = joblib.load(model_path)
+
+    elif model_type == 'MLP':
+        model = MLPClassifier(solver='lbfgs', hidden_layer_sizes=hidden_layer)
+
+    spaces, answers = loader.load(dataset_dir)
+    trained_model = model.fit(spaces, answers)
+
+    joblib.dump(trained_model, output, protocol=0)
+
+
+if __name__ == '__main__':
+    train()
